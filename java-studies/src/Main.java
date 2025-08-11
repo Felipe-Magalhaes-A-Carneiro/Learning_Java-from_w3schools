39e5316be0e88935da4cf90d806e448bee07b4bf
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2025-08-11 11:29:16 -0300

    studies update 01

---
+++ java-studies/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    // In Java, every application begins with a class name, and that class must match the filename.
+    // NOTE: Every line of code that runs in Java must be inside a class. And the class name should always start with an uppercase first letter. In our example, we named the class Main.
+
+    // The main() method is required and you will see it in every Java program:
+
+    public static void main(String[] args) throws Exception {
+        // NOTE: The curly braces {} marks the beginning and the end of a block of code.
+        System.out.println("Hello, World!");
+        
+        //each code statement must end with a semicolon (;)
+    }
+}
